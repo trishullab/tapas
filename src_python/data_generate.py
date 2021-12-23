f5201f81d52ac933b2a5fe02c2c48eb723a4009f
Thomas Logan <thomas.logan.atx@gmail.com>
2021-12-22 18:07:12 -0600

    parallelize data generation

--- src_python/data_generate.py
+++ src_python/data_generate.py
@@ -1,12 +1,13 @@
 from __future__ import annotations
 from lib import data
 
-# data.generate_dir('mbpp')
+if __name__ == '__main__':
+    # data.generate_dir('mbpp')
 
-# data.generate_dir('apps')
+    # data.generate_dir('apps')
 
-# data.generate_dir('py150_train')
-# data.generate_dir('py150_test')
-# data.generate_dir('py150_validation')
+    # data.generate_dir('py150_train')
+    # data.generate_dir('py150_test')
+    # data.generate_dir('py150_validation')
 
-data.generate_dir('cubert')
+    data.generate_dir('cubert')
--- src_python/lib/data.py
+++ src_python/lib/data.py
@@ -19,185 +19,197 @@ from gen.instance import Vocab
 
 base_path = pathlib.Path(__file__).parent.absolute()
 
-def generate_dir(dirname):
+
+
+
+def generate_file(dirname : str, name : str, vocab : dict):
+
+    concrete_data_dirpath = os.path.join(base_path, f"../../res/{dirname}/concrete_data")
+    abstract_data_dirpath = os.path.join(base_path, f"../../res/{dirname}/abstract_data")
+
+    logging.basicConfig(level=logging.INFO)
+
+    concrete_data_path = os.path.join(concrete_data_dirpath, name)
+
+    abstract_data_base = name.split(".")[0]  
+    # write(abstract_data_dirpath, f'{abstract_data_base}_training.txt', '')
+    write(abstract_data_dirpath, f'{abstract_data_base}_json.txt', '')
+    write(abstract_data_dirpath, f'{abstract_data_base}_stats.txt', '')
+
+    from datetime import datetime
+
+    start = datetime.now()
+
+    with open(concrete_data_path, 'r') as f:
+        processed_count = 0
+        rec_error_count = 0
+        error_count = 0
+        total_count = 0
+        line = f.readline()
+        while line: 
+
+            line_obj = json.loads(line)
+
+            source_code = line_obj['code']
+
+            tree = generic_tree.parse('python', source_code, 'utf8')
+
+
+            instances : list[instance] = []
+
+            try:
+                mod = from_generic_ast(tree)
+                # mod = python_ast_rename.rename_Module(mod, {}, {}, {})
+
+                instances = serialize_Module(mod)
+
+                # if total_count < 50:
+
+                    # print(f"-------------------------")
+                    # print(f"line number: {total_count + 1}")
+                    # print(f"-------------------------")
+
+                    # concrete_code = python_instance.concretize(instances)
+
+                    # print(f"-------------------------")
+                    # print(f"generic tree:")
+                    # print(generic_tree.dump(tree))
+
+                    # print(f"-------------------------")
+                    # print(f"production tree:")
+                    # print(python_instance.dump(instances))
+
+                    # print(f"-------------------------")
+                    # print(f"source:")
+                    # print(source_code)
+
+                    # print(f"-------------------------")
+                    # print(f"concretized:")
+                    # print(concrete_code)
+
+
+
+                def triple_from_instance(inst : instance) -> tuple[str, str, str]:
+                    return match_instance(inst, InstanceHandlers[tuple[str, str, str]](
+                        case_Grammar=lambda o : (
+                            ("grammar", o.options, o.selection)
+                        ),
+                        case_Vocab=lambda o : (
+                            ("vocab", o.options, o.selection)
+                        )
+                    )) 
+
+
+                # data = [
+                #     f"[{t[0]},{t[1]},{t[2]}]"
+                #     for i in instances
+                #     for t in [triple_from_instance(i)] 
+                # ]
+                # write(abstract_data_dirpath, f'{abstract_data_base}_training.txt', "[" + ",".join(data) + "]" + '\n\n<|endoftext|>\n\n', append=True)
+
+
+                json_data = []
+                for i in instances:
+                    triple = triple_from_instance(i)
+                    for ti in triple:
+                        json_data.append(ti)
+
+                write(abstract_data_dirpath, f'{abstract_data_base}_json.txt', json.dumps(json_data) + '\n\n<|endoftext|>\n\n', append=True)
+
+
+                def handle_Vocab(o : Vocab):
+                    if o.options in vocab.keys():
+                        vocab[o.options].add(o.selection)
+                    else:
+                        vocab[o.options] = {o.selection}
+
+                # update vocabulary
+                for inst in instances:
+
+                    match_instance(inst, InstanceHandlers(
+                        case_Grammar=lambda o : (),
+                        case_Vocab=handle_Vocab 
+                    )) 
+                
+                processed_count += 1
+
+            except RecursionError:
+                rec_error_count += 1
+                error_count += 1
+                # print(f"\n\n")
+
+                # print(f"""--Generic Tree--\n{
+                #     generic_tree.dump(tree, 
+                #         text_cond = lambda n : len(n.children) == 0 or n.syntax_part == "string"
+                #     )
+                # }\n""")
+
+                # print(f"\n\n")
+                # print(f"---Source Code---")
+                # print(source_code)
+                # print(f"-------------------------")
+                # print(f"recursion error line number: {count}")
+                # print("RECURSION ERROR")
+                # return
+
+            except Exception as x:
+                error_count += 1
+                # print(f"\n-------------------------\n")
+                # print(f"""--Generic Tree--\n{
+                #     generic_tree.dump(tree, 
+                #         text_cond = lambda n : len(n.children) == 0 or n.syntax_part == "string"
+                #     )
+                # }\n""")
+
+
+                # print(f"\n-------------------------\n")
+                # print(f"---Source Code---")
+                # print(source_code)
+
+                # print(f"\n-------------------------\n")
+                # print(f"line number: {count}")
+
+
+                # raise x
+
+            # update
+            line = f.readline()
+            total_count += 1
+
+        #endwhile
+
+    end = datetime.now()
+
+    stats = \
+    f"""
+        id: {dirname} :: {name}"
+        time: {end - start}
+        total count: {total_count}
+        processed count: {processed_count}
+        error count: {error_count}
+        rec error count: {rec_error_count}
+    """
+
+    write(abstract_data_dirpath, f'{abstract_data_base}_stats.txt', stats)
+
+
+def generate_file_tuple(triple):
+    generate_file(triple[0], triple[1], triple[2])
+
+def generate_dir(dirname : str):
     concrete_data_dirpath = os.path.join(base_path, f"../../res/{dirname}/concrete_data")
     vocab : dict[str, set[str]] = {}
 
     abstract_data_dirpath = os.path.join(base_path, f"../../res/{dirname}/abstract_data")
     write(abstract_data_dirpath, f'vocab.json', '')
 
-    def generate_file(name : str):
-        nonlocal concrete_data_dirpath
-        nonlocal vocab
 
-        logging.basicConfig(level=logging.INFO)
+    import multiprocessing
+    pool = multiprocessing.Pool(multiprocessing.cpu_count())
+    pool.map(generate_file_tuple, [(dirname, n, vocab) for n in os.listdir(concrete_data_dirpath)])
 
-        concrete_data_path = os.path.join(concrete_data_dirpath, name)
-
-        abstract_data_base = name.split(".")[0]  
-        # write(abstract_data_dirpath, f'{abstract_data_base}_training.txt', '')
-        write(abstract_data_dirpath, f'{abstract_data_base}_json.txt', '')
-        write(abstract_data_dirpath, f'{abstract_data_base}_stats.txt', '')
-
-        from datetime import datetime
-
-        start = datetime.now()
-
-        with open(concrete_data_path, 'r') as f:
-            processed_count = 0
-            rec_error_count = 0
-            error_count = 0
-            total_count = 0
-            line = f.readline()
-            while line: 
-
-                line_obj = json.loads(line)
-
-                source_code = line_obj['code']
-
-                tree = generic_tree.parse('python', source_code, 'utf8')
-
-
-                instances : list[instance] = []
-
-                try:
-                    mod = from_generic_ast(tree)
-                    # mod = python_ast_rename.rename_Module(mod, {}, {}, {})
-
-                    instances = serialize_Module(mod)
-
-                    # if total_count < 50:
-
-                        # print(f"-------------------------")
-                        # print(f"line number: {total_count + 1}")
-                        # print(f"-------------------------")
-
-                        # concrete_code = python_instance.concretize(instances)
-
-                        # print(f"-------------------------")
-                        # print(f"generic tree:")
-                        # print(generic_tree.dump(tree))
-
-                        # print(f"-------------------------")
-                        # print(f"production tree:")
-                        # print(python_instance.dump(instances))
-
-                        # print(f"-------------------------")
-                        # print(f"source:")
-                        # print(source_code)
-
-                        # print(f"-------------------------")
-                        # print(f"concretized:")
-                        # print(concrete_code)
-
-
-
-                    def triple_from_instance(inst : instance) -> tuple[str, str, str]:
-                        return match_instance(inst, InstanceHandlers[tuple[str, str, str]](
-                            case_Grammar=lambda o : (
-                                ("grammar", o.options, o.selection)
-                            ),
-                            case_Vocab=lambda o : (
-                                ("vocab", o.options, o.selection)
-                            )
-                        )) 
-
-
-                    # data = [
-                    #     f"[{t[0]},{t[1]},{t[2]}]"
-                    #     for i in instances
-                    #     for t in [triple_from_instance(i)] 
-                    # ]
-                    # write(abstract_data_dirpath, f'{abstract_data_base}_training.txt', "[" + ",".join(data) + "]" + '\n\n<|endoftext|>\n\n', append=True)
-
-
-                    json_data = []
-                    for i in instances:
-                        triple = triple_from_instance(i)
-                        for ti in triple:
-                            json_data.append(ti)
-
-                    write(abstract_data_dirpath, f'{abstract_data_base}_json.txt', json.dumps(json_data) + '\n\n<|endoftext|>\n\n', append=True)
-
-
-                    def handle_Vocab(o : Vocab):
-                        if o.options in vocab.keys():
-                            vocab[o.options].add(o.selection)
-                        else:
-                            vocab[o.options] = {o.selection}
-
-                    # update vocabulary
-                    for inst in instances:
-
-                        match_instance(inst, InstanceHandlers(
-                            case_Grammar=lambda o : (),
-                            case_Vocab=handle_Vocab 
-                        )) 
-                    
-                    processed_count += 1
-
-                except RecursionError:
-                    rec_error_count += 1
-                    error_count += 1
-                    # print(f"\n\n")
-
-                    # print(f"""--Generic Tree--\n{
-                    #     generic_tree.dump(tree, 
-                    #         text_cond = lambda n : len(n.children) == 0 or n.syntax_part == "string"
-                    #     )
-                    # }\n""")
-
-                    # print(f"\n\n")
-                    # print(f"---Source Code---")
-                    # print(source_code)
-                    # print(f"-------------------------")
-                    # print(f"recursion error line number: {count}")
-                    # print("RECURSION ERROR")
-                    # return
-
-                except Exception as x:
-                    error_count += 1
-                    # print(f"\n-------------------------\n")
-                    # print(f"""--Generic Tree--\n{
-                    #     generic_tree.dump(tree, 
-                    #         text_cond = lambda n : len(n.children) == 0 or n.syntax_part == "string"
-                    #     )
-                    # }\n""")
-
-
-                    # print(f"\n-------------------------\n")
-                    # print(f"---Source Code---")
-                    # print(source_code)
-
-                    # print(f"\n-------------------------\n")
-                    # print(f"line number: {count}")
-
-
-                    # raise x
-
-                # update
-                line = f.readline()
-                total_count += 1
-
-            #endwhile
-
-        end = datetime.now()
-
-        stats = \
-        f"""
-            id: {dirname} :: {name}"
-            time: {end - start}
-            total count: {total_count}
-            processed count: {processed_count}
-            error count: {error_count}
-            rec error count: {rec_error_count}
-        """
-
-        write(abstract_data_dirpath, f'{abstract_data_base}_stats.txt', stats)
-
-
-    for filename in os.listdir(concrete_data_dirpath):
-        generate_file(filename)
+    # for filename in os.listdir(concrete_data_dirpath):
+    #     generate_file(filename)
 
     write(abstract_data_dirpath, f'vocab.json', json.dumps(
         {
