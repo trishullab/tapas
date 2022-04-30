0280a56feb3edbce8f7295fbdb1bc81391bce93a
Thomas Logan <thomas.logan.atx@gmail.com>
2022-04-30 17:26:00 -0500

    update

--- src/build_grammars.py
+++ src/build_grammars.py
@@ -4,19 +4,16 @@ import os
 import pathlib
 # import logging
 
+from lib import util_system as us
 # logging.basicConfig(level=logging.INFO)
-base_path = pathlib.Path(__file__).parent.absolute()
-
-def relative(rel_path : str) -> str:
-    return os.path.join(base_path, rel_path)
 
 Language.build_library(
     # Store the library in the `build` directory
-    relative('../build/grammars.so'),
+    us.project_path('build/grammars.so'),
 
     # Include one or more languages
     [
         # 'vendor/tree-sitter-python'
-        relative('../../tree-sitter-python/')
+        us.project_path('../tree-sitter-python/')
     ]
 )
