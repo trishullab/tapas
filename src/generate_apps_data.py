5299ea4da3ba4f31b0030e276be56e3e726ac2c5
Thomas Logan <thomas.logan.atx@gmail.com>
2023-01-05 22:43:02 -0600

    update

--- src/generate_apps_data.py
+++ src/generate_apps_data.py
@@ -47,10 +47,9 @@ if __name__ == "__main__":
             i = problem['problem_id']
             file_name = f'{name}_{str(i).zfill(5)}.jsonl'
             solutions = json.loads(problem['solutions'])
-            data = [
+            data = map(lambda solution : 
                 lambda:python_data_system.add_semantic_data(python_ast_system.serialize(python_ast_system.parse(solution)), package)
-                for solution in solutions
-            ]
+            , solutions)
             new_count_map = python_data_system.write_data(abstract_data_dirpath, file_name, data)
             count_map = merge_count_map(count_map, new_count_map)
         return count_map
